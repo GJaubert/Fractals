2c7b29e313e60bf388b872f9759d1b5e9dbf2687
Gabriel Garcia Jaubert <ggjaubert@gmail.com>
2023-06-08 19:40:34 +0200

    color change added

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,6 @@ using namespace fractal;
 
 int main() {
 
-	RGB rgb1(4, 5, 6);
-	RGB rgb2(1, 2, 3);
-
-	RGB rgb3 = rgb1 - rgb2;
-
-	cout << rgb3.r << rgb3.g << rgb3.b << endl;
-
 	FractalCreator fractalCreator(800, 600);
 	fractalCreator.run("test.bmp");
 
