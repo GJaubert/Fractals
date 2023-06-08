73272e057dad065bb59e62bf29c5eafd5118c370
Gabriel Garcia Jaubert <ggjaubert@gmail.com>
2023-06-08 19:35:30 +0200

    run function implemented

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,16 @@ using namespace std;
 using namespace fractal;
 
 int main() {
-	int const WIDTH = 800;
-	int const HEIGHT = 600;
+
+	RGB rgb1(4, 5, 6);
+	RGB rgb2(1, 2, 3);
+
+	RGB rgb3 = rgb1 - rgb2;
+
+	cout << rgb3.r << rgb3.g << rgb3.b << endl;
 
 	FractalCreator fractalCreator(800, 600);
-
-	fractalCreator.addZoom(Zoom(295, HEIGHT - 202, 0.1));
-	fractalCreator.addZoom(Zoom(312, HEIGHT - 304, 0.1));
-	fractalCreator.calculateIterations();
-	fractalCreator.calculateTotalIterations();
-	fractalCreator.drawFractal();
-	fractalCreator.writeBitmap("test.bmp");
+	fractalCreator.run("test.bmp");
 
 	cout << "Finished";
 	return 0;
