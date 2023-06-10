9b0fa39967e80068c43a2923ac235855a43dc415
Gabriel Garcia Jaubert <ggjaubert@gmail.com>
2023-06-10 20:10:15 +0200

    getRange added

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ int main() {
 	fractalCreator.addRange(0.5, RGB(255, 255,  0));
 	fractalCreator.addRange(1.0, RGB(255, 255,  255));
 
+	cout << fractalCreator.getRange(400);
+
 	fractalCreator.addZoom(Zoom(295, 202, 0.1));
 	fractalCreator.addZoom(Zoom(312, 304, 0.1));
 	fractalCreator.run("test.bmp");
